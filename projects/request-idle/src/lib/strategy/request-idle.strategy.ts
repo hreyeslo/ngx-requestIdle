bf530d416e49ba506de9cc9b9ff3e896262a09b4
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2019-11-30 18:55:28 +0100

    Added preload strategy

---
+++ projects/request-idle/src/lib/strategy/request-idle.strategy.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { PreloadingStrategy, Route } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import 'requestidlecallback-polyfill';
+
+import { AbstractRequestIdleService } from '../service/abstract-request-idle.service';
+
+@Injectable()
+export class RequestIdlePreloadAllModules implements PreloadingStrategy {
+  constructor(private _reuqestIdleService: AbstractRequestIdleService) { }
+
+  preload(route: Route, load: () => Observable<any>): Observable<any> {
+    this._reuqestIdleService.callback(() => load());
+    return of(null);
+  }
+}
--- projects/request-idle/src/public-api.ts
+++ projects/request-idle/src/public-api.ts
@@ -3,5 +3,6 @@
  */
 
 export { AbstractRequestIdleService as RequestIdle } from './lib/service/abstract-request-idle.service';
+export { RequestIdlePreloadAllModules } from './lib/strategy/request-idle.strategy';
 export { RequestIdleOptions } from './lib/service/request-idle.model';
 export { RequestIdleModule } from './lib/request-idle.module';
